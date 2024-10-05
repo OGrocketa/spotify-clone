1cedca707848cbad0be288a42a55ec6c5dd329f3
OGrocketa <jaroslaw.sedach@gmail.com>
2024-10-05 21:56:03 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import PlayerBar from './components/PlayerBar'
+
+function App() {
+
+  return (
+    <>
+        
+        <PlayerBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/PlayerBar.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import "./PlayerBar.css"
+
+function PlayerBar(){
+
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const togglePlayPause = () => {
+        setIsPlaying(!isPlaying);
+    }
+
+    return(
+        <>
+            <div className="player-bar">
+
+                <div className="song-info">
+                    <img className= "album-cover" src="" alt="Song cover" />
+                    <div className="song-details">
+                        <p className="song-title">Song name</p>
+                        <p className="song-artist">Artist Name</p>
+                    </div>
+                </div>
+                <div className="player-control-and-progress-bar">
+                    <div className="player-controls">
+                        <button className="prev-button">⏮</button>
+                        <button className="play-pause-button" onClick={togglePlayPause}> {isPlaying ?'⏸' : '▶️' }</button>
+                        <button className="next-button">⏭</button>
+                    </div>
+
+                    <div className="song-progress">
+                        <p>0:15</p>
+                        <input className= "playback-progress-bar" type="range" min={0} max={100} value={10}/>
+                        <p>2:25</p>
+                    </div>
+                </div>
+                
+            </div>
+        </>
+    );
+}
+
+export default PlayerBar
