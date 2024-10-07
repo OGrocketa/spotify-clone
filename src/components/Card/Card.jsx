7ec6da2e6a3e4195347a373aa2853a93bb341e1f
OGrocketa <145462153+OGrocketa@users.noreply.github.com>
2024-10-07 20:53:29 +0200

    Update Card.jsx

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,13 @@
 import "./Card.css"
 
 
-const Card = ({imagePath, cardTitle, additionalInfo,customImageClass }) => {
+const Card = ({imagePath, cardTitle, additionalInfo,isRound }) => {
     
+    const imageClass = isRound ? "round-image" : "";
+
     return(
         <div className="card-container">
-            <img src={imagePath} alt="" />
+            <img src={imagePath} alt={cardTitle} className= {imageClass} />
             <div className="card-info">
                 <h1 className="cardTitle">{cardTitle}</h1>
                 <p className="additionalInfo">{additionalInfo}</p>
