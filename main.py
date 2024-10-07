1247da5f828e9c59f1d1210324955530254fa856
OGrocketa <jaroslaw.sedach@gmail.com>
2024-10-07 11:27:38 +0200

    updated imgae serve logic

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 import json
 from fastapi.middleware.cors import CORSMiddleware
-
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
@@ -17,14 +17,20 @@ app.add_middleware(
 with open('public/data/albums.json') as file:
     albums = json.load(file)
 
+app.mount("/artists", StaticFiles(directory="public/artists"), name="artists")
+
+
 @app.get("/song/{title}")
 def get_song_by_title(title:str):
     try:
         for album in albums:
             for song in album['songs']:
                 if song['title'].lower() == title.lower():
+                    
+                    album_cover = f"/artists/{album['artistName']}/albums/{album['title']}/cover.png"
+
                     return {"song": song,
-                            "albumName": album['title'],
+                            "artistName": album['artistName'],
                             "artistId": album['artistId'],
                             "albumId" :album['id'],
                             "albumCover": album['cover'],
