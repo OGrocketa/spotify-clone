e2df5c155fa20d439edf9c3937219f7f86836826
OGrocketa <jaroslaw.sedach@gmail.com>
2024-10-20 18:37:25 +0200

    Update ArtistBadge.jsx

--- src/components/ArtistBadge/ArtistBadge.jsx
+++ src/components/ArtistBadge/ArtistBadge.jsx
@@ -1,7 +1,15 @@
 import "./ArtistBadge.css"
 
-const ArtistBadge = (artist) => {
-    
+const ArtistBadge = ({artist}) => {
+    return(
+        <div className="artist-badge-container">
+            <img src={artist.avatar} alt="artist avatar" />
+            <div>
+                <p>{artist.type}</p>
+                <p>{artist.name}</p>
+            </div>
+        </div>
+    );
 }
 
 export default ArtistBadge
