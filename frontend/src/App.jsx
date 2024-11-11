5013efc42cac1f77eb7943ec98e6ef7047ef7d90
OGrocketa <jaroslaw.sedach@gmail.com>
2024-11-11 14:00:09 +0100

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ let swimming = '1f693a7f-6054-4ff6-a8a9-bd8341274c36';
 let artist_id = '99770812-4218-46cd-86d1-e65d06a7ddc1';
 // const songs = await fetchSongsFromAlbum('1e4fbdec-6df7-4e96-be84-7c4b113230b8');
 // const song = await fetchSong('0e3098e2-9876-11ef-999d-512ce8086d0d');
-console.log(fetchTopSongsByArtist(artist_id));
+// console.log(fetchTopSongsByArtist(artist_id));
 
 
 function App() {
