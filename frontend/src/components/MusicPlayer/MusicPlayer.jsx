24e46240522b1820252b4b988f5cb1fec4715a9f
OGrocketa <jaroslaw.sedach@gmail.com>
2024-11-25 12:03:24 +0100

    Update MusicPlayer.jsx

--- frontend/src/components/MusicPlayer/MusicPlayer.jsx
+++ frontend/src/components/MusicPlayer/MusicPlayer.jsx
@@ -6,61 +6,69 @@ import { FaCirclePause,FaCirclePlay } from "react-icons/fa6";
 
 
 const MusicPlayer = () =>{
-    const audioRef = useRef();
-    
-    const{
-        id,
-        file_url,
-        title,
-        isPlaying,
-        name,
-        cover_url,
-        setPlayerData,
-        setPlay,
-    } = usePlayer();
+  const audioRef = useRef();
+  
+  const{
+      id,
+      file_url,
+      title,
+      isPlaying,
+      name,
+      cover_url,
+      setPlayerData,
+      setPlay,
+  } = usePlayer();
 
-    const handlePlaySong = (song_id)=>{
-      usePlayerFetchSong(song_id);
-    }
-    
-    
-    
-
-    useEffect(() => {
-        if (audioRef.current && file_url) {
-          if (isPlaying) {
-            audioRef.current.play();
-          } else {
-            audioRef.current.pause();
-          }
+  const handlePlaySong = (song_id)=>{
+    usePlayerFetchSong(song_id);
+  }
+  
+  useEffect(() => {
+    if (audioRef.current && file_url) {
+        audioRef.current.pause(); // Pause current playback
+        audioRef.current.load(); // Reload the audio element with the new source
+        if (isPlaying) {
+            audioRef.current.play(); // Start playback for the new song
         }
-      }, [isPlaying]);
+    }
+  }, [file_url]); 
+  
 
-      return (
-        <div className='music-player-container'>
-         
-            <div className='player-song-info'>
-              <img src={cover_url} alt="Album Cover" />
-              <div className="song-title-and-artist-container">
-                <span className='song-title'>{title}</span>
-                <span className='song-artist'>{name}</span>
-              </div>
+  useEffect(() => {
+      if (audioRef.current && file_url) {
+        if (isPlaying) {
+          audioRef.current.play();
+        } else {
+          audioRef.current.pause();
+        }
+      }
+    }, [isPlaying]);
+
+    return (
+      <div className='music-player-container'>
+        
+          <div className='player-song-info'>
+            <img src={cover_url} alt="Album Cover" />
+            <div className="song-title-and-artist-container">
+              <span className='song-title'>{title}</span>
+              <span className='song-artist'>{name}</span>
             </div>
+          </div>
 
-            <div className='music-main-controls'>
-              <button onClick={setPlay} className='player-play-pause-button'>
-                {isPlaying ? <FaCirclePause size={30}/> :  <FaCirclePlay size={30}/>}
-              </button>
-            </div>
+          <div className='music-main-controls'>
+            <button onClick={setPlay} className='player-play-pause-button'>
+              {isPlaying ? <FaCirclePause size={30}/> :  <FaCirclePlay size={30}/>}
+            </button>
+          </div>
 
-           <div className='player-music-add-controls'>
-            
-           </div>
+          <div className='player-music-add-controls'>
           
-          <audio ref={audioRef} src={file_url}/>
-        </div>
-      );
-      
+          </div>
+        
+        <audio ref={audioRef} src={file_url}/>
+      </div>
+    );
+    
 };
 
 export default MusicPlayer;
