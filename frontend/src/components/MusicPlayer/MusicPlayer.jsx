a187da38b7f02ff86d6d6685dc81f766d70a4875
OGrocketa <jaroslaw.sedach@gmail.com>
2024-11-27 13:03:17 +0100

    Sldier works good

--- frontend/src/components/MusicPlayer/MusicPlayer.jsx
+++ frontend/src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,113 +1,124 @@
-import { useEffect, useRef, useState } from 'react';
-import usePlayer from '../../hooks/usePlayer';
-import usePlayerFetchSong from '../../hooks/usePlayerFetchSong';
-import './MusicPlayer.css'
-import { FaCirclePause,FaCirclePlay,FaForwardStep,FaBackwardStep } from "react-icons/fa6";
-import usePlayNewSong from '../../hooks/usePlayNewSong';
-import MusicSlider from '../Slider/MusicSlider';
+  import { useEffect, useRef, useState } from 'react';
+  import usePlayer from '../../hooks/usePlayer';
+  import usePlayerFetchSong from '../../hooks/usePlayerFetchSong';
+  import './MusicPlayer.css'
+  import { FaCirclePause,FaCirclePlay,FaForwardStep,FaBackwardStep } from "react-icons/fa6";
+  import usePlayNewSong from '../../hooks/usePlayNewSong';
+  import MusicSlider from '../Slider/MusicSlider';
 
 
-const MusicPlayer = () =>{
-  const audioRef = useRef();
-  
-  const{
-      ids,
-      curId,
-      file_url,
-      title,
-      isPlaying,
-      name,
-      cover_url,
-      setPlayerIds,
-      setPlayerSongData,
-      setPlay,
-  } = usePlayer();
+  const MusicPlayer = () =>{
+    const audioRef = useRef();
+    
+    const{
+        ids,
+        curId,
+        file_url,
+        title,
+        isPlaying,
+        name,
+        cover_url,
+        setPlayerIds,
+        setPlayerSongData,
+        setPlay,
+    } = usePlayer();
 
-  const [currentTime, setCurrentTime] = useState(0)
-  const [duration, setDuration] = useState(0);
+    const [currentTime, setCurrentTime] = useState(0)
+    const [duration, setDuration] = useState(0);
 
-  const fetchAndSetSong = usePlayerFetchSong(); 
+    const fetchAndSetSong = usePlayerFetchSong(); 
 
-  //Handle playback when song changes
-  usePlayNewSong(audioRef, file_url, isPlaying);
+    //Handle playback when song changes
+    usePlayNewSong(audioRef, file_url, isPlaying);
 
-  //Handle Play Pause
-  useEffect(() => {
-      if (audioRef.current && file_url) {
-        if (isPlaying) {
-          audioRef.current.play();
-        } else {
-          audioRef.current.pause();
+    //Handle Play Pause
+    useEffect(() => {
+        if (audioRef.current && file_url) {
+          if (isPlaying) {
+            audioRef.current.play();
+          } else {
+            audioRef.current.pause();
+          }
+        }
+      }, [isPlaying]);
+
+      
+      const playNext = () =>{
+        
+        const curIndex = ids.findIndex((index) => curId == index);
+        if(ids[curIndex + 1]){
+          const nextSongId = ids[curIndex + 1];
+            fetchAndSetSong(nextSongId);
+        }
+      };
+
+      const playPrev = ()=>{
+        const curIndex = ids.findIndex((index) => curId == index);
+        if(ids[curIndex - 1]){
+          const prevSongId = ids[curIndex - 1];
+            fetchAndSetSong(prevSongId);
+        }
+      };
+
+      const handleTimeUpdate = () => {
+        setCurrentTime(audioRef.current.currentTime);
+      };
+
+      const handleLoadSong = () => {
+        setDuration(audioRef.current.duration);
+      };
+
+      const handleUserSliderChange = (value) =>{
+        const newTime = value[0];
+        audioRef.current.currentTime = newTime;
+        setCurrentTime(newTime);
+
+      }
+
+      const handleSongEndedPlayNext = () =>{
+        if(audioRef.current.ended){
+          playNext();
         }
       }
-    }, [isPlaying]);
-
     
-    const playNext = () =>{
-      
-      const curIndex = ids.findIndex((index) => curId == index);
-      if(ids[curIndex + 1]){
-        const nextSongId = ids[curIndex + 1];
-          fetchAndSetSong(nextSongId);
-      }
-    };
 
-    const playPrev = ()=>{
-      const curIndex = ids.findIndex((index) => curId == index);
-      if(ids[curIndex - 1]){
-        const prevSongId = ids[curIndex - 1];
-          fetchAndSetSong(prevSongId);
-      }
-    };
-
-    const handleTimeUpdate = () => {
-      setCurrentTime(audioRef.current.currentTime);
-    };
-
-    const handleLoadSong = () => {
-      setDuration(audioRef.current.duration);
-    };
-
-    const handleUserSliderChange = (value) =>{
-      const newTime = value[0];
-      audioRef.current.currentTime = newTime;
-      setCurrentTime(newTime);
-
-    }
-  
-
-    return (
-      <div className='music-player-container'>
-        
-          <div className='player-song-info'>
-            <img src={cover_url} alt="" />
-            <div className="song-title-and-artist-container">
-              <span className='song-title'>{title}</span>
-              <span className='song-artist'>{name}</span>
-            </div>
-          </div>
-
-          <div className='music-main-controls'>
-            <div className='music-main-controls-buttons'>
-              <button onClick={playPrev} className='player-fast-forward'> <FaBackwardStep size={20}/></button>
-              <button onClick={setPlay} className='player-play-pause-button'>
-                {isPlaying ? <FaCirclePause size={30}/> :  <FaCirclePlay size={30}/>}
-              </button>
-              <button onClick={playNext} className='player-fast-forward'>
-                <FaForwardStep size={20}/> 
-              </button>
-            </div>
-            <MusicSlider currentTime={currentTime} duration={duration} onValueCommit={handleTimeUpdate}/>
-          </div>
-
-          <div className='player-music-add-controls'>
+      return (
+        <div className='music-player-container'>
           
-          </div>
-        
-        <audio ref={audioRef} src={file_url} onLoadedMetadata={handleLoadSong} onTimeUpdate={handleTimeUpdate}/>
-      </div>
-    );
-    
-};
+            <div className='player-song-info'>
+              <img src={cover_url} alt="" />
+              <div className="song-title-and-artist-container">
+                <span className='song-title'>{title}</span>
+                <span className='song-artist'>{name}</span>
+              </div>
+            </div>
 
-export default MusicPlayer;
+            <div className='music-main-controls'>
+              <div className='music-main-controls-buttons'>
+                <button onClick={playPrev} className='player-fast-forward'> <FaBackwardStep size={20}/></button>
+                <button onClick={setPlay} className='player-play-pause-button'>
+                  {isPlaying ? <FaCirclePause size={30}/> :  <FaCirclePlay size={30}/>}
+                </button>
+                <button onClick={playNext} className='player-fast-forward'>
+                  <FaForwardStep size={20}/> 
+                </button>
+              </div>
+              <div className='music-slider-container'>
+                <p></p>
+                <MusicSlider currentTime={currentTime} duration={duration} onValueChange={handleUserSliderChange}/>
+                {/* <p>{duration}</p> */}
+              </div>
+            
+            </div>
+
+            <div className='player-music-add-controls'>
+            
+            </div>
+          
+          <audio ref={audioRef} src={file_url} onLoadedMetadata={handleLoadSong} onTimeUpdate={handleTimeUpdate} onEnded={handleSongEndedPlayNext}/>
+        </div>
+      );
+      
+  };
+
+  export default MusicPlayer;
--- frontend/src/components/Slider/MusicSlider.jsx
+++ frontend/src/components/Slider/MusicSlider.jsx
@@ -1,10 +1,10 @@
 import * as Slider from "@radix-ui/react-slider";
 import './MusicSlider.css'
-const MusicSlider = ({currentTime, duration,onValueCommit})=>{
+const MusicSlider = ({currentTime, duration,onValueChange})=>{
 
 
     return(
-        <Slider.Root className="SliderRoot"  max={duration} step={1} value={[currentTime]} onValueCommit={onValueCommit} >
+        <Slider.Root className="SliderRoot"  max={duration} step={1} value={[currentTime]} onValueChange={onValueChange} >
             <Slider.Track className="SliderTrack">
                 <Slider.Range className="SliderRange"  />
             </Slider.Track>
