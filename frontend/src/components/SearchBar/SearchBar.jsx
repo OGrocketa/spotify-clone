824ab79a2566a6435a9c578269f4c260f3903973
OGrocketa <jaroslaw.sedach@gmail.com>
2024-12-14 15:28:57 +0100

    search bar upd

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,26 +1,35 @@
 import React from "react";
 import './SearchBar.css'
-import { GoHome } from "react-icons/go";
+import { GoHome,GoHomeFill  } from "react-icons/go";
 import { Link } from "react-router-dom";
-
+import { useLocation } from "react-router-dom";
 
 const SearchBar = () =>{
+
+    const path = useLocation();
+    const isOnMainPage = path.pathname == '/';
+    console.log(isOnMainPage);
+
     return(
         <>
         <div className="search-bar-container">
-            <div className="logo-container">
-                <img src="/images/logo.png" alt="" />   
-            </div>
-
-            <Link to={`/`}>
-                <button className='go-home-button'>
-                    <GoHome size={30}/>
-                </button>
-            </Link>
             
+                <div className="logo-container">
+                    <Link to={`/`}>
+                        <img src="/images/logo.png" alt="" />   
+                    </Link>
+                </div>
+           
+            
+            <div className="center-of-sbc">
+                <Link to={`/`}>
+                    <button className="go-home-button" title="Go to Home">
+                        {isOnMainPage? <GoHomeFill size={30}/> : <GoHome size={30}/> }
+                    </button>
+                </Link>
 
             <input type="text" className="search-input" placeholder="Search for songs, artists, albums..." />
-
+            </div>
             
         </div>
         
