177e6c1f7a80e885fbb7f333c23acd7e1500dff4
OGrocketa <jaroslaw.sedach@gmail.com>
2024-12-14 15:00:44 +0100

    added home button

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import './SearchBar.css'
+import { GoHome } from "react-icons/go";
+import { Link } from "react-router-dom";
+
 
 const SearchBar = () =>{
     return(
@@ -8,6 +11,13 @@ const SearchBar = () =>{
             <div className="logo-container">
                 <img src="/images/logo.png" alt="" />   
             </div>
+
+            <Link to={`/`}>
+                <button className='go-home-button'>
+                    <GoHome size={30}/>
+                </button>
+            </Link>
+            
 
             <input type="text" className="search-input" placeholder="Search for songs, artists, albums..." />
 
