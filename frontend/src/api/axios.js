bc1856cc6aeffe7e3b49abfeae8e211bbf56f8d6
OGrocketa <jaroslaw.sedach@gmail.com>
2025-01-02 15:27:07 +0100

    Update axios.js

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { create } from "zustand";
 const BASE_URL = 'http://127.0.0.1:8000/'
 
 export const axiosPrivate = axios.create({
@@ -7,3 +6,4 @@ export const axiosPrivate = axios.create({
     withCredentials:true,
     headers: {"Content-Type":"application/json"}
 })
+
