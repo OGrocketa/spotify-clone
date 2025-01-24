68ac801c7f47f83c27934573a1657b845d322e69
Yaraslau Sedach <jaroslaw.sedach@gmail.com>
2025-01-24 16:43:54 +0100

    Create PersistLogin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,25 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {Routes, Route } from 'react-router-dom';
 import AlbumProfile from './pages/AlbumProfile/AlbumProfile.jsx'
 import SongProfile from './pages/SongProfile/SongProfile.jsx'
 import ArtistProfile from './pages/ArtistProfile/ArtistProfile.jsx'
 import HomePage from './pages/HomePage/HomePage.jsx';
-
+import PersistLogin from './components/PersistLogin.jsx'
 import Layout from '../Layout.jsx';
+
+const Roles = {
+    'User' : 0,
+    "Moderator" : 1,
+    "Admin" : 2
+  }
+
 
 function App() {
   
   return (
         <Routes>
           <Route path='/' element={<Layout/>}>
+          {/* Public routes */}
               <Route path='/' element={<HomePage/>}/>
               <Route path="/artist/:artist_id" element={<ArtistProfile />} />
               <Route path="/album/:album_id" element={<AlbumProfile />} />
