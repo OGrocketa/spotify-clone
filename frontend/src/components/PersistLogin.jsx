63331a9672c87a98e153bd250eac7b9bb8352bd1
Yaraslau Sedach <jaroslaw.sedach@gmail.com>
2025-01-24 16:33:09 +0100

    Create PersistLogin.jsx

---
+++ frontend/src/components/PersistLogin.jsx
@@ -0,0 +1,38 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+
+const PersistLogin = () =>{
+    const [isLoading, setIsLoading] = useState(true);
+    const refresh = useRefreshToken();
+    const {auth} = useAuth();
+
+    useEffect(()=>{
+        const verifyRefreshToken = async() =>{
+            try{
+                await refresh();
+            }catch(error){
+                console.error(error);
+            }finally{
+                setIsLoading(false);
+            }
+        }
+        !auth?.access_token ? verifyRefreshToken() : setIsLoading(false);
+    },[]);
+
+    return(
+        <>
+        {
+            isLoading
+            ?
+            <p>Loading ...</p>
+            :
+            <Outlet/>
+        }
+        </>
+    );
+
+}
+
+export default PersistLogin;
